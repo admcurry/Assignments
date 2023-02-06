3df3cb47041e8c7d4eaad3af12b1f8115f2f8222
Adam Curry <124198480+atcurry@users.noreply.github.com>
2023-02-06 12:43:08 +0000

    Create Q1.R

---
+++ Quizzes/Q1.R
@@ -0,0 +1,23 @@
+dbinom(1, 4, 0.2)
+dbinom(2, 8, 0.2)
+dbinom(10, 40, 0.2)
+
+
+1 - pbinom(4, 29, 2/38)
+dbinom(0, 29, 2/38)
+1 - pbinom(13, 29, 18/38)
+pbinom(12, 29, 18/38)
+
+
+pbinom(0, 36, 0.13)
+pbinom(4, 36, 0.13)
+1 - pbinom(2, 36, 0.13)
+
+
+dbinom(9, 9, 0.65)
+dbinom(3, 9, 0.65)
+
+dbinom(0:9, 9, 0.65)
+barplot(dbinom(0:9, 9, 0.65))
+
+dbinom()
